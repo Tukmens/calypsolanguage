c246e57b7e09d449d5d9c9ce52aff04e1767bd56
Tukmens <tomish25@gmail.com>
2018-02-27 21:50:02 +0200

    Add files via upload

---
+++ ACHIEV.php
@@ -0,0 +1,397 @@
+<?php //Achievements
+
+$LNG['ach_head']                     		= 'Achievements';
+$LNG['ach_sys']                     		= 'System';
+$LNG['ach_achieved']                     	= '(Achieved)';
+$LNG['ach_lvl']                      		= 'lvl.';
+$LNG['ach_pointes']                    		= 'Achievement Points';
+$LNG['ach_bonus_mes']                    	= '<img alt="" style="float:left; width:60px; margin-right:6px;" src="styles/%s" >Reached: <span class="achiev_mes_head">%s %s level.</span><br> Received:<br> %s Antimatter and <br> %s Achievement Points';
+$LNG['ach_requirements']               		= 'Requirements';
+$LNG['ach_next_bonus']               		= 'Bonus at the next level: <br> &bull; %s Antimatter <br> &bull; %s Achievement Points';
+
+//Группы
+$LNG['ach_group']['general']				= 'General';
+$LNG['ach_group']['build']					= 'Buildings';
+$LNG['ach_group']['tech']					= 'Research';
+$LNG['ach_group']['fleet']					= 'Fleet';
+$LNG['ach_group']['def']					= 'Defense';
+$LNG['ach_group']['varia']					= 'Miscellaneous';
+$LNG['ach_group']['daily']					= 'Daily';
+
+//-------------------------------Постройки
+$LNG['ach_mine_metal'] = array(
+	'name'			=> 'Metal Miner',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Metal mine %s lvl.',
+	),	
+);
+$LNG['ach_crystal_mine'] = array(
+	'name'			=> 'Crystal Miner',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Crystal mine %s lvl.',
+	),	
+);
+$LNG['ach_deuterium_sintetizer'] = array(
+	'name'			=> 'Deuterium Miner',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Deuterium mine %s lvl.',
+	),	
+);
+$LNG['ach_conveyor1'] = array(
+	'name'			=> 'Small Producer',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Small production facillity %s lvl.',
+	),	
+);
+$LNG['ach_conveyor2'] = array(
+	'name'			=> 'Medium Producer',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Medium production facillity %s lvl.',
+	),	
+);
+$LNG['ach_conveyor3'] = array(
+	'name'			=> 'Large Producer',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Large production facillity %s lvl.',
+	),	
+);
+$LNG['ach_university'] = array(
+	'name'			=> 'Mad Scientist',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'University %s lvl.',
+	),	
+);
+$LNG['ach_mondbasis'] = array(
+	'name'			=> 'Moon Coloniser',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Moon base %s lvl.',
+	),	
+);
+$LNG['ach_phalanx'] = array(
+	'name'			=> 'Phalanx Sensor',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Phalanx sensor %s lvl.',
+	),	
+);
+$LNG['ach_terraformer'] = array(
+	'name'			=> 'Terraformer',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Terraformer %s lvl.',
+	),	
+);
+//////////////////////////////////ИССЛЕДОВАНИЯ
+$LNG['ach_spy_tech'] = array(
+	'name'			=> 'Spy',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Espionage %s lvl.',
+	),	
+);
+$LNG['ach_computer_tech'] = array(
+	'name'			=> 'Hacker',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Computer technology %s lvl.',
+	),	
+);
+$LNG['ach_war_tech'] = array(
+	'name'			=> 'Unbeatable technology',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Weapons technology %s lvl.',
+		'Shield technology %s lvl.',
+		'Armor Technology %s lvl.',
+	),	
+);
+$LNG['ach_expedition_tech'] = array(
+	'name'			=> 'Expedition members',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Astrophysics %s lvl.',
+	),	
+);
+$LNG['ach_gravity_tech'] = array(
+	'name'			=> 'Gravitation',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Gravitational technology %s lvl.',
+	),	
+);
+$LNG['ach_gun_tech'] = array(
+	'name'			=> 'The power of guns',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Laser technology %s lvl.',
+		'Ionic technology %s lvl.',
+		'Plasma technology %s lvl.',
+	),	
+);
+$LNG['ach_energy_tech'] = array(
+	'name'			=> 'Energetic',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Energy technology %s lvl.',
+	),	
+);
+$LNG['ach_bank_ally_tech'] = array(
+	'name'			=> 'Brotherhood',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Brotherhood %s lvl.',
+	),	
+);
+$LNG['ach_motor_tech'] = array(
+	'name'			=> 'Increase the Speed',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Jet engine %s lvl.',
+		'Impulse engine %s lvl.',
+		'Hyperspace engineь %s lvl.',
+	),	
+);
+$LNG['ach_mining_tech'] = array(
+	'name'			=> 'Geologist',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Metal extraction technology %s lvl.',
+		'Crystal extraction technology %s lvl.',
+		'Deuterium extraction technology %s lvl.',
+	),	
+);	
+/////////////////// Флот
+
+$LNG['ach_hunter_fleet'] = array(
+	'name'			=> 'Fleet Fighters',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Light fighters %s units.',
+		'Heavy fighter %s units.',
+	),
+);
+
+$LNG['ach_support_fleet'] = array(
+	'name'			=> 'Fleet Support',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Cruisers %s units.',
+		'Battleships %s units.',
+	),
+);
+
+$LNG['ach_battle_fleet'] = array(
+	'name'			=> 'Battle fleet',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Star fighter %s units.',
+		'Battle cruiser %s units.',
+	),
+);
+
+$LNG['ach_destruction_fleet'] = array(
+	'name'			=> 'Fleet Destruction',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Death Star %s units.',
+		'Galleon %s units.',
+	),
+);
+
+$LNG['ach_siege_fleet'] = array(
+	'name'			=> 'Fleet Siege',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Planet Bomber %s units.',
+		'Destroyers %s units.',
+	),
+);
+
+$LNG['ach_heavy_fleet'] = array(
+	'name'			=> 'Heavy fleet',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Frigate %s units.',
+		'Black Wanderer %s units.',
+	),
+);
+
+$LNG['ach_emperor_fleet'] = array(
+	'name'			=> 'Imperial fleet',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Avatar %s units.',
+		'Flying death %s units.',
+		'Battleships class ONill %s units.',
+	),
+);
+
+
+/////////////////// Оборона
+$LNG['ach_light_def'] = array(
+	'name'			=> 'Light defense',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Missile Launcher %s units.',
+		'Light Laser Turret %s units.',
+	),
+);
+
+$LNG['ach_easy_def'] = array(
+	'name'			=> 'Simple defense',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Heavy Laser Turret %s units.',
+		'Ion Cannon %s units.',
+	),
+);
+
+$LNG['ach_medium_def'] = array(
+	'name'			=> 'Average defense',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Gauss Cannon %s units.',
+		'Plasma Cannon %s units.',
+	),
+);
+
+$LNG['ach_high_def'] = array(
+	'name'			=> 'High defense',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Hydrogen Gun %s units.',
+		'Dora gun %s units.',
+	),
+);
+
+$LNG['ach_heavy_def'] = array(
+	'name'			=> 'Heavy defense',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Gravitons Cannon %s units.',
+		'Proton Gun %s units.',
+	),
+);
+
+$LNG['ach_top_def'] = array(
+	'name'			=> 'Massive defense',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Canyon %s units.',
+		'Quantum Gun %s units.',
+	),
+);
+
+$LNG['ach_emperor_def'] = array(
+	'name'			=> 'Imperial defense',
+	'text'			=> '',
+	'requirements' 	=> array( // требования
+		'Proton Gun %s units.',
+		'Particle Emitter %s units.',
+		'Lepton Gun %s units.',
+	),
+);
+
+/////////////////// ОБЩИЕ
+$LNG['ach_level'] = array(
+	'name'			=> 'Peaceful',
+	'text'			=> 'The reward will come out for the achievement of each 5 peace levels',
+	'requirements' 	=> array( // требования
+		'Peace level %s .',
+	),
+);
+$LNG['ach_batle_level'] = array(
+	'name'			=> 'Veteran',
+	'text'			=> 'The reward will come out for the achievement of each  5 batlle levels',
+	'requirements' 	=> array( // требования
+		'Battle level %s .',
+	),
+);
+
+/////////////////// РАЗНОЕ
+$LNG['ach_wons'] = array(
+	'name'			=> 'Fighter',
+	'text'			=> 'To achiev you need to win 50 fights against players.',
+	'requirements' 	=> array( // требования
+		'Winned %s battles.',
+	),
+);
+$LNG['ach_destroyer_moons'] = array(
+	'name'			=> 'Moon destroyer',
+	'text'			=> 'To achiev you must destroy 3 enemy moon.',
+	'requirements' 	=> array( // требования
+		'Destroyed %s moons.',
+	),
+);
+$LNG['ach_creation_moons'] = array(
+	'name'			=> 'Mon maker',
+	'text'			=> 'To achiev, you must create 3 moons in the fighting.',
+	'requirements' 	=> array( // требования
+		'Creater %s moons.',
+	),
+);
+$LNG['ach_wons_em'] = array(
+	'name'			=> 'Victory Hostal',
+	'text'			=> 'To achiev you must win 20 battles in Hostal sector',
+	'requirements' 	=> array( // требования
+		'Winned %s battles in Hostal sector.',
+	),
+);
+$LNG['ach_expedition'] = array(
+	'name'			=> 'Successfull expo',
+	'text'			=> 'To achiev you need 10 successful flights in the expedition.',
+	'requirements' 	=> array( // требования
+		'%s successful flights in expedition.',
+	),
+);
+$LNG['ach_found_tm'] = array(
+	'name'			=> 'Dark matter finder',
+	'text'			=> 'To achiev  need to find 50,000 Dark Matter',
+	'requirements' 	=> array( // требования
+		'Found %s Dark Matter.',
+	),
+);
+$LNG['ach_found_up'] = array(
+	'name'			=> 'Upgrade finder',
+	'text'			=> 'To accomplish achieve must find 3 upgrade',
+	'requirements' 	=> array( // требования
+		'Found %s Upgrades.',
+	),
+);
+$LNG['ach_action_up'] = array(
+	'name'			=> 'Upgrade engeneer',
+	'text'			=> 'To achieve  must successfully activate the 10 upgrades.',
+	'requirements' 	=> array( // требования
+		'Activised %s Upgrades.',
+	),
+);
+/////////////////// ЕЖЕДНЕВНЫЕ
+$LNG['ach_wons_day'] = array(
+	'name'			=> 'Raider',
+	'text'			=> 'To achiev need to win 15 battles against the player.',
+	'requirements' 	=> array( // требования
+		'Winned %s battles.',
+	),
+);
+$LNG['ach_wons_em_day'] = array(
+	'name'			=> 'Victory Hostal / twenty-four hours',
+	'text'			=> 'To achiev need to win 20 battles in Hostal sector.',
+	'requirements' 	=> array( // требования
+		'Winned %s battles in Hostal sector.',
+	),
+);
+$LNG['ach_expedition_day'] = array(
+	'name'			=> 'Successful expo / twenty-four hours',
+	'text'			=> 'To achiev need 20 successful flight in the expedition.',
+	'requirements' 	=> array( // требования
+		'%s successful flights in expedition.',
+	),
+);
